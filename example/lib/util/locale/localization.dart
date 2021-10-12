6e97de46a5aa9f3860a9e851ee994b14e0d4bfe2
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-10-12 13:32:08 +0200

    Updated to the latest version of locale_gen including some bugfixes update translations at runtime with LocalizationOverrides

--- example/lib/util/locale/localization.dart
+++ example/lib/util/locale/localization.dart
@@ -3,46 +3,64 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:icapps_translations_example/util/locale/localization_keys.dart';
+import 'package:icapps_translations_example/util/locale/localization_overrides.dart';
 
 //============================================================//
 //THIS FILE IS AUTO GENERATED. DO NOT EDIT//
 //============================================================//
 class Localization {
-  Map<String, dynamic> _localisedValues = Map();
+  var _localisedValues = <String, dynamic>{};
+  var _localisedOverrideValues = <String, dynamic>{};
 
   static Localization of(BuildContext context) =>
       Localizations.of<Localization>(context, Localization)!;
 
-  static Future<Localization> load(Locale locale,
-      {bool showLocalizationKeys = false}) async {
-    final localizations = Localization();
+  /// The locale is used to get the correct json locale.
+  /// It can later be used to check what the locale is that was used to load this Localization instance.
+  final Locale locale;
+
+  Localization({required this.locale});
+
+  static Future<Localization> load(
+    Locale locale, {
+    LocalizationOverrides? localizationOverrides,
+    bool showLocalizationKeys = false,
+    bool useCaching = true,
+  }) async {
+    final localizations = Localization(locale: locale);
     if (showLocalizationKeys) {
       return localizations;
     }
-    final jsonContent = await rootBundle
-        .loadString('assets/localization/${locale.languageCode}.json');
-    // ignore: avoid_as
+    if (localizationOverrides != null) {
+      final overrideLocalizations =
+          await localizationOverrides.getOverriddenLocalizations(locale);
+      localizations._localisedOverrideValues = overrideLocalizations;
+    }
+    final jsonContent = await rootBundle.loadString(
+        'assets/localization/${locale.languageCode}.json',
+        cache: useCaching);
     localizations._localisedValues =
-        json.decode(jsonContent) as Map<String, dynamic>;
+        json.decode(jsonContent) as Map<String, dynamic>; // ignore: avoid_as
     return localizations;
   }
 
   String _t(String key, {List<dynamic>? args}) {
     try {
-      // ignore: avoid_as
-      var value = _localisedValues[key] as String;
-      if (value == null) return '$key';
+      final value =
+          (_localisedOverrideValues[key] ?? _localisedValues[key]) as String?;
+      if (value == null) return key;
       if (args == null || args.isEmpty) return value;
-      args
-          .asMap()
-          .forEach((index, arg) => value = _replaceWith(value, arg, index + 1));
-      return value;
+      var newValue = value;
+      // ignore: avoid_annotating_with_dynamic
+      args.asMap().forEach((index, dynamic arg) =>
+          newValue = _replaceWith(newValue, arg, index + 1));
+      return newValue;
     } catch (e) {
       return '⚠$key⚠';
     }
   }
 
-  String _replaceWith(String value, arg, argIndex) {
+  String _replaceWith(String value, Object? arg, int argIndex) {
     if (arg == null) return value;
     if (arg is String) {
       return value.replaceAll('%$argIndex\$s', arg);
@@ -52,18 +70,61 @@ class Localization {
     return value;
   }
 
+  /// Translations:
+  ///
+  /// en:  **'Testing in English'**
+  ///
+  /// nl:  **'Test in het Nederlands'**
   String get test => _t(LocalizationKeys.test);
 
-  String testArg1(String arg1) => _t(LocalizationKeys.testArg1, args: [arg1]);
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 string]'**
+  ///
+  /// nl:  **'Test argument [arg1 string]'**
+  String testArg1(String arg1) =>
+      _t(LocalizationKeys.testArg1, args: <dynamic>[arg1]);
 
-  String testArg2(num arg1) => _t(LocalizationKeys.testArg2, args: [arg1]);
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 number]'**
+  ///
+  /// nl:  **'Test argument [arg1 number]'**
+  String testArg2(num arg1) =>
+      _t(LocalizationKeys.testArg2, args: <dynamic>[arg1]);
 
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 string] [arg2 number]'**
+  ///
+  /// nl:  **'Test argument [arg1 string] [arg2 number]'**
   String testArg3(String arg1, num arg2) =>
-      _t(LocalizationKeys.testArg3, args: [arg1, arg2]);
+      _t(LocalizationKeys.testArg3, args: <dynamic>[arg1, arg2]);
 
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 string] [arg2 number] [arg1 string]'**
+  ///
+  /// nl:  **'Test argument [arg1 string] [arg2 number] [arg1 string]'**
   String testArg4(String arg1, num arg2) =>
-      _t(LocalizationKeys.testArg4, args: [arg1, arg2]);
+      _t(LocalizationKeys.testArg4, args: <dynamic>[arg1, arg2]);
+
+  /// Translations:
+  ///
+  /// en:  **'Testing\nargument\n\n[arg1 string] [arg2 number] [arg1 string]'**
+  ///
+  /// nl:  **'Test\nargument\n\n[arg1 string] [arg2 number] [arg1 string]'**
+  String testNewLine(String arg1, num arg2) =>
+      _t(LocalizationKeys.testNewLine, args: <dynamic>[arg1, arg2]);
+
+  /// Translations:
+  ///
+  /// en:  **'Carriage\r\nReturn'**
+  ///
+  /// nl:  **'Carriage\r\nReturn'**
+  String get testNewLineCarriageReturn =>
+      _t(LocalizationKeys.testNewLineCarriageReturn);
 
   String getTranslation(String key, {List<dynamic>? args}) =>
-      _t(key, args: args ?? []);
+      _t(key, args: args ?? <dynamic>[]);
 }
--- example/lib/util/locale/localization_keys.dart
+++ example/lib/util/locale/localization_keys.dart
@@ -2,13 +2,52 @@
 //THIS FILE IS AUTO GENERATED. DO NOT EDIT//
 //============================================================//
 class LocalizationKeys {
+  /// Translations:
+  ///
+  /// en:  **'Testing in English'**
+  ///
+  /// nl:  **'Test in het Nederlands'**
   static const test = 'test';
 
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 string]'**
+  ///
+  /// nl:  **'Test argument [arg1 string]'**
   static const testArg1 = 'test_arg1';
 
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 number]'**
+  ///
+  /// nl:  **'Test argument [arg1 number]'**
   static const testArg2 = 'test_arg2';
 
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 string] [arg2 number]'**
+  ///
+  /// nl:  **'Test argument [arg1 string] [arg2 number]'**
   static const testArg3 = 'test_arg3';
 
+  /// Translations:
+  ///
+  /// en:  **'Testing argument [arg1 string] [arg2 number] [arg1 string]'**
+  ///
+  /// nl:  **'Test argument [arg1 string] [arg2 number] [arg1 string]'**
   static const testArg4 = 'test_arg4';
+
+  /// Translations:
+  ///
+  /// en:  **'Testing\nargument\n\n[arg1 string] [arg2 number] [arg1 string]'**
+  ///
+  /// nl:  **'Test\nargument\n\n[arg1 string] [arg2 number] [arg1 string]'**
+  static const testNewLine = 'test_new_line';
+
+  /// Translations:
+  ///
+  /// en:  **'Carriage\r\nReturn'**
+  ///
+  /// nl:  **'Carriage\r\nReturn'**
+  static const testNewLineCarriageReturn = 'test_new_line_carriage_return';
 }
